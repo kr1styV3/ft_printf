0b211345a9360a7bd1c169888a7dc12a086753c7
olramazz <oliverramazzotti03@gmail.com>
2023-12-28 18:24:21 +0100

    Added ft_putnbr_out

---
+++ ft_putnbr.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr_fd.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: olramazz <olramazz@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/29 01:36:46 by olramazz          #+#    #+#             */
+/*   Updated: 2023/11/29 01:36:46 by olramazz         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <unistd.h>
+#include <stddef.h>
+
+// int main()
+// {
+//     // Example usage of ft_putnbr_fd to print different numbers to stdout
+//     ft_putnbr_fd(123, 1);  // Print "123"
+//     ft_putchar_fd('\n', 1); // New line for readability
+//     ft_putnbr_fd(-456, 1);  // Print "-456"
+//     ft_putchar_fd('\n', 1); // New line for readability
+//     ft_putnbr_fd(0, 1);    // Print "0"
+//     ft_putchar_fd('\n', 1); // New line for readability
+//     ft_putnbr_fd(2147483647, 1); // Print "2147483647" (max int value)
+//     ft_putchar_fd('\n', 1);      // New line for readability
+//     ft_putnbr_fd(-2147483648, 1); // Print "-2147483648" (min int value)
+//     ft_putchar_fd('\n', 1);       // New line for readability
+//     return 0;
+// }
